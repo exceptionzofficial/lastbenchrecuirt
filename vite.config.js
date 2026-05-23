59f8d1a985131ce7a2217e4338eaaa70f4ff63e1
exceptionzofficial <exceptionzofficial@gmail.com>
2026-05-23 14:12:07 +0530

    fix: Support standalone Vercel build by dynamically setting outDir and base path

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/recruiter/',
+  base: process.env.VERCEL ? '/' : '/recruiter/',
   build: {
-    outDir: '../recruiter',
+    outDir: process.env.VERCEL ? 'dist' : '../recruiter',
     emptyOutDir: true
   },
   server: {
